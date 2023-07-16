060904731bc9cdc61c5d3744bf26b6db8f96a34f
Richard Autry <autry.richard@gmail.com>
2023-07-16 16:40:02 -0400

    Added id tplinker library for device scanning and manipulation

--- src/greetings.h
+++ src/greetings.h
@@ -2,3 +2,5 @@
 
 const char* rust_greeting(const char* to);
 void rust_greeting_free(char *);
+const char* *tplinker_discovery(unsigned int *len);
+void tplinker_vec_destroy(int8_t *arr);
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,10 @@
-// use tplinker::{
-//     discovery::discover,
-//     devices::Device,
-//     capabilities::Switch,
-// };
-
-// fn main() {
-//     for (addr, data) in discover().unwrap() {
-//         let device = Device::from_data(addr, &data);
-//         let sysinfo = data.sysinfo();
-//         println!("{}\t{}\t{}", addr, sysinfo.alias, sysinfo.hw_type);
-//     }
-// }
-use std::os::raw::{c_char};
+use std::os::raw::{c_char, c_uint};
 use std::ffi::{CString, CStr};
+use tplinker::{
+    discovery::discover,
+    devices::Device,
+    capabilities::Switch,
+};
 
 #[no_mangle]
 pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
@@ -31,4 +23,38 @@ pub extern fn rust_greeting_free(s: *mut c_char) {
         if s.is_null() { return }
         CString::from_raw(s)
     };
+}
+
+// fn main() {
+//     for (addr, data) in discover().unwrap() {
+//         let device = Device::from_data(addr, &data);
+//         let sysinfo = data.sysinfo();
+//         println!("{}\t{}\t{}", addr, sysinfo.alias, sysinfo.hw_type);
+//     }
+// }
+
+#[no_mangle]
+pub extern "C" fn tplinker_discovery(len: *mut c_uint) -> *mut c_char {
+    let mut device_descriptions = Vec::new();
+    for (addr, data) in discover().unwrap() {
+        let device = Device::from_data(addr, &data);
+        let sysinfo = data.sysinfo();
+        // let device_description = CString::new("{addr}\t{sysinfo.alias}\t{sysinfo.hw_type}").unwrap();
+        let device_description = CString::new(format!("{}\t{}\t{}", addr, sysinfo.alias, sysinfo.hw_type)).unwrap();
+        device_descriptions.push(device_description);
+    }
+    let slice = device_descriptions.into_boxed_slice();
+    unsafe {
+        *len = slice.len() as c_uint;
+    }
+    // unsafe {
+    //     *len = slice.len() as c_uint;
+    // }
+
+    Box::into_raw(slice) as *mut c_char
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tplinker_vec_destroy(tplinker_vec: *mut i8) {
+    let _ = Box::from_raw(tplinker_vec);
 }
