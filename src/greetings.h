e17f63367245c64a58001cfbcca1f4c0c5e3ddaf
Richard Autry <autry.richard@gmail.com>
2023-07-20 13:38:17 -0400

    Device discovery and reading via raw pointers now working

--- src/greetings.h
+++ src/greetings.h
@@ -1,6 +1,9 @@
 #include <stdint.h>
 
+struct device_data;
+
 const char* rust_greeting(const char* to);
 void rust_greeting_free(char *);
-const char* *tplinker_discovery(unsigned int *len);
+const struct device*_Nonnull *tplinker_discovery(unsigned int *len);
 void tplinker_vec_destroy(int8_t *arr);
+const char* _Nonnull device_data_get_alias(const struct device_data* _Nonnull device_data);
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use std::os::raw::{c_char, c_uint};
+use std::os::raw::{c_char, c_uint, c_void};
 use std::ffi::{CString, CStr};
 use tplinker::{
     discovery::discover,
     devices::Device,
-    capabilities::Switch,
+    capabilities::DeviceActions,
+    datatypes::DeviceData
 };
 
 #[no_mangle]
@@ -34,27 +35,44 @@ pub extern fn rust_greeting_free(s: *mut c_char) {
 // }
 
 #[no_mangle]
-pub extern "C" fn tplinker_discovery(len: *mut c_uint) -> *mut c_char {
-    let mut device_descriptions = Vec::new();
+pub extern "C" fn tplinker_discovery(len: *mut c_uint) -> *mut c_void {
+    //let mut device_descriptions = Vec::new();
+    let mut device_data_objects: Vec<*mut _> = Vec::new();
     for (addr, data) in discover().unwrap() {
         let device = Device::from_data(addr, &data);
         let sysinfo = data.sysinfo();
-        // let device_description = CString::new("{addr}\t{sysinfo.alias}\t{sysinfo.hw_type}").unwrap();
-        let device_description = CString::new(format!("{}\t{}\t{}", addr, sysinfo.alias, sysinfo.hw_type)).unwrap();
-        device_descriptions.push(device_description);
+        // let device_description = CString::new(format!("{}\t{}\t{}", addr, sysinfo.alias, sysinfo.hw_type)).unwrap();
+        //let device_description: (CString, CString) = (CString::new(format!("{}", addr)).unwrap(), CString::new(format!("{}", sysinfo.alias)).unwrap());
+        // let device_description: [CString; 2] = [CString::new(format!("{}", addr)).unwrap(), CString::new(format!("{}", sysinfo.alias)).unwrap()];
+        //device_descriptions.push(device_description);
+        // TODO: Need a way to expose device data and allow calling functions on those devices
+        // Will need to expose device name and pointer to allow function calls on those devices
+        device_data_objects.push(Box::into_raw(Box::new(data)))
     }
-    let slice = device_descriptions.into_boxed_slice();
+    let slice = device_data_objects.into_boxed_slice();
     unsafe {
         *len = slice.len() as c_uint;
     }
-    // unsafe {
-    //     *len = slice.len() as c_uint;
-    // }
-
-    Box::into_raw(slice) as *mut c_char
+    // let boxed = Box::new(device_data_objects);
+    
+    // TODO: Is this return wrong or incompatible?
+    Box::into_raw(slice) as *mut c_void
+    // Box::into_raw(boxed) as *mut c_void
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn tplinker_vec_destroy(tplinker_vec: *mut i8) {
     let _ = Box::from_raw(tplinker_vec);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn device_data_get_alias(device_data: *const DeviceData) -> *mut c_char {
+    let device_data = &*device_data;
+    println!("RETURNING ALIAS");
+    // let device_data: DeviceData = device.send("{}").unwrap();
+    let sys_info = device_data.sysinfo();
+    // let alias = sys_info.alias.clone();
+    // let alias: String = device_data.sysinfo().alias.clone();
+    // println!("RETURNING {:?} FOR DEVICE", alias);
+    CString::new(sys_info.mac.clone()).unwrap().into_raw()
 }
